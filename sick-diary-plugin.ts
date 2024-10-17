1fb0251f893cd1c79983b5e74dbbea27f0512faa
bulany <bulany.git@gmail.com>
2024-10-17 17:26:48 +0200

    Parse basic sick values

--- sick-diary-plugin.ts
+++ sick-diary-plugin.ts
@@ -1,12 +1,49 @@
 
 import { Plugin, Notice, MarkdownPostProcessorContext } from 'obsidian';
 
+
+function parseSimpleYaml(input : string) {
+  const lines = input.split('\n');
+  const output : any = {};
+  lines.forEach(line => {
+    const [key, value] = line.split(': ');
+    if (key && value)
+      output[key] = value;
+  })
+  return output;
+}
+
+class SickValue {
+  start: string = ''
+  end: string = ''
+  sickness: number = 0
+  summary: string = ''
+}
+
+function validateSickValue(obj:any) {
+
+  const outObj : any = {};
+  if (!obj.start)
+    return null
+  outObj.start = obj.start;
+  if (!obj.end)
+    return null
+  outObj.end = obj.end;
+  if (!obj.sickness)
+    return null
+  outObj.sickness = obj.sickness;
+  outObj.summary = obj.summary ? obj.summary : ''
+  const output = outObj as SickValue;
+  return output;
+}
+
 export class SickDiaryPlugin {
   plugin : Plugin | null = null;
+  values : Array<SickValue> = [];
 
 	async onload(plugin : Plugin) {
     this.plugin = plugin;
-    this.plugin.registerMarkdownCodeBlockProcessor('sick', this.processCodeBlock);
+    this.plugin.registerMarkdownCodeBlockProcessor('sick', this.processDataBlock);
 		console.log('sick diary onload');
 	}
 
@@ -14,7 +51,7 @@ export class SickDiaryPlugin {
 		console.log('sick diary onunload');
 	}
 
-  processCodeBlock(source: string, 
+  processDataBlock(source: string, 
     el: HTMLElement, 
     ctx: MarkdownPostProcessorContext)
   {
@@ -22,6 +59,11 @@ export class SickDiaryPlugin {
     const code = pre.createEl('code');
     code.textContent = source;
     console.log('process1', source);
+    const obj = parseSimpleYaml(source);
+    const val = validateSickValue(obj);
+    if (val) {
+      console.log('we have a value', val);
+    }
 
   }
 }
